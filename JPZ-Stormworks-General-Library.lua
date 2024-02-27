2301b0866fb77f42c1955415bfbecfa9edcf442f
SlopeSlayer910 <SlopeSlayer910@gmail.com>
2024-02-27 16:55:59 +0200

    Updating the parameters for some of the functions

--- JPZ-Stormworks-General-Library.lua
+++ JPZ-Stormworks-General-Library.lua
@@ -3,25 +3,33 @@ function clamp(min, max, value)
     return math.max(min, math.min(max, value))
   end
 --#endregion
+
 --#region turnsToDegrees
 function turnsToDegrees(turns)
     return turns*360
 end
 --#endregion
+
 --#region turnsToBearing
 function turnsToBearing(turns)
     return ((1+turns)%1)*360
 end
 --#endregion
---#region cartesianToPolar
-function cartesianToPolar(X,Y)
-    local angle, distance = math.atan(X,-Y), math.sqrt(X^2+Y^2)
+
+--Converts cartesian coordinates to polar coordinates
+---@param x number The angle for conversion (North 0, Clockwise, radians)
+---@param y number The distance for conversion
+---@return number angle, number distance The x and y value of the coverted coordinate
+function cartesianToPolar(x,y)
+    local angle, distance = math.atan(x,-y), math.sqrt(x^2+y^2)
     return angle, distance
 end
---#endregion
---#region polarToCartesian
+
+--Converts polar coordinates to cartesian coordinates
+---@param angle number The angle for conversion (North 0, Clockwise, radians)
+---@param distance number The distance for conversion
+---@return number x, number y The x and y value of the coverted coordinate
 function polarToCartesian(angle,distance)
     local x, y = distance*math.sin(angle), -distance*math.cos(angle)
     return x, y
 end
---#endregion
